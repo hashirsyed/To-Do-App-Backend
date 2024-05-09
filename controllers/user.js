84a813f9a3cfc13964905cda5c58a010079b3e1c
hashirsyed <itshashirsyed@gmail.com>
2024-05-09 16:38:19 +0500

    Fixed some mistakes

--- controllers/user.js
+++ controllers/user.js
@@ -8,7 +8,7 @@ module.exports = {
   signUp: async function (req, res) {
     try {
       const { name, email, password, profileUrl } = req.body;
-      if (!name && !email && !password && !profileUrl) {
+      if (!name && !email && !password ) {
         throw generateErrorInstance({
           status: 404,
           message: "Required fields cannot be empty",
@@ -22,7 +22,10 @@ module.exports = {
       });
 
       if(user){
-        return res.status(409).send("User already exists with this email");
+        throw generateErrorInstance({
+          status: 409,
+          message: "User already exist with this email",
+        });
       }
       
       const hashedPassword = await bcrypt.hash(password,12)
@@ -36,7 +39,6 @@ module.exports = {
       const token = jwt.sign(user, config.get("jwt_secret"), {
         expiresIn: "1d",
       });
-      console.log(token)
       
       res.status(201).send({user,token});
     } catch (err) {
