5698e49241bcf2c4052bd5bd562615a7597d9894
hashirsyed <itshashirsyed@gmail.com>
2024-06-01 08:54:45 +0500

    Created DeleteTask API successfully

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -79,5 +79,34 @@ module.exports = {
       res.status(500).send(err.message || "Something went wrong!");
     }
   },
+  delete : async function (req, res) {
+    try {
+      const { taskId } = req.params;
+  
+      if (!taskId) {
+        throw generateErrorInstance({
+          status: 404,
+          message: "Task ID is required",
+        });
+      }
+  
+      let task = await Tasks.findByPk(taskId);
+  
+      if (!task) {
+        return res.status(404).send("Task not found");
+      }
+  
+      await task.destroy({
+        where : {
+          id : taskId
+        }
+      });
+  
+      res.status(200).send({ message: "Task deleted successfully" });
+    } catch (err) {
+      console.log(err);
+      res.status(500).send(err.message || "Something went wrong!");
+    }
+  },
   
 };
--- routes/tasks.js
+++ routes/tasks.js
@@ -11,5 +11,6 @@ const auth = require("../middlewares/auth");
 router.post("/:userId/tasks" , auth , controller.create);
 router.get("/:userId/tasks", auth , controller.getAll);
 router.put("/:userId/tasks/:taskId", auth , controller.edit);
+router.delete("/:userId/tasks/:taskId", auth , controller.delete);
 
 module.exports = router;
